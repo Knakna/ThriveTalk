cd99d56706d5ff79c85aeddfa29025ec30515051
knakna <kna@tut.by>
2021-04-30 22:18:37 +0300

    add site structure

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+// https://micku7zu.github.io/vanilla-tilt.js/
+
+VanillaTilt.init(document.querySelector(".about__img"), {
+    max: 25,
+    speed: 400
+});
+
+
+// https://wowjs.uk/
+
+new WOW().init();
+
+
+
