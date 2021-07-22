5c6c1ff3bed7f89d8fbc9aeed48af45df1ce75ca
knakna <kna@tut.by>
2021-07-22 17:12:19 +0300

    add adaptive to box, know, contact, info, footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 
-
 // open/close nav
 
 $(document).ready(function () {
-    $(".burger").click(function (e) {
-        $(".burger__line,.header__nav").toggleClass('active');
-        $("body").toggleClass('lock');
-    })
-  });
+  $(".burger").click(function (e) {
+    $(".burger__line,.header__nav").toggleClass('active');
+    $("body").toggleClass('lock');
+  })
+});
+
+
+//   active class switching for btns
+
+$('.header__boxes').on('click', '.header__button', function () {
+  $(this).addClass('active').siblings().removeClass('active');
+});
+
 
 
 
